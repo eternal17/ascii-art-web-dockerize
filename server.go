2d25a9faa682a1f980a0e6034f7d56247a5e5fc7
tb38r <tlawal86@hotmail.co.uk>
2021-11-29 19:47:44 +0000

    added text box et al

--- server.go
+++ server.go
@@ -14,24 +14,64 @@ type Banner struct {
 	Ban3  string
 }
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("static/*.html"))
+}
+
 func bannerHandler(w http.ResponseWriter, r *http.Request) {
+
 	p := Banner{
 		Title: "SELECT BANNERFILE\n",
 		Ban1:  "Shadow\n",
 		Ban2:  "Standard\n",
 		Ban3:  "Thinkertoy\n",
 	}
+
+	//tpl.ExecuteTemplate(w, "index.gohtml", nil)
+
 	t, _ := template.ParseFiles("static/index.html")
 	t.Execute(w, p)
+
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
 
-	fmt.Fprintf(w, "<h1>Step by Step</h1>")
+	fmt.Fprintf(w, "<h1>ASCII WEB hOMEPAGE</h1>")
+}
+
+func processHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	getban1 := r.FormValue("banner")
+	getban2 := r.FormValue("banner")
+	getban3 := r.FormValue("banner")
+	tbox := r.FormValue("textbox")
+
+	testReturn := struct {
+		ban1    string
+		ban2    string
+		ban3    string
+		textbox string
+	}{
+		ban1:    getban1,
+		ban2:    getban2,
+		ban3:    getban3,
+		textbox: tbox,
+	}
+
+	fmt.Printf("%v", testReturn)
+
+
 }
 
 func main() {
@@ -39,9 +79,10 @@ func main() {
 	//server := http.FileServer(http.Dir("./static"))
 
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/banner", bannerHandler)
+	http.HandleFunc("/process", processHandler)
+	http.HandleFunc("/banner/", bannerHandler)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 
 	}
